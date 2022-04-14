64e6d69edfc27c0e05220bb437fa34e3c29b1489
Andrea Colussi <andreacolussi00@gmail.com>
2022-04-14 12:36:51 +0200

    Add project

---
+++ ACEasyDate/ACEasyDate/ACEasyDate.h
@@ -0,0 +1,18 @@
+//
+//  ACEasyDate.h
+//  ACEasyDate
+//
+//  Created by Andrea Colussi on 14/04/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ACEasyDate.
+FOUNDATION_EXPORT double ACEasyDateVersionNumber;
+
+//! Project version string for ACEasyDate.
+FOUNDATION_EXPORT const unsigned char ACEasyDateVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ACEasyDate/PublicHeader.h>
+
+
